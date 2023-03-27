bf886a230e785cd9e3c96eaef470a41267085cd9
mafumaful <s6bian@uwaterloo.ca>
2023-03-27 00:25:48 -0400

    default

--- test.py
+++ test.py
@@ -1,23 +1,34 @@
 import gym
 import random
 
+# Create the environment
 env = gym.make('CartPole-v1', render_mode="human")
 env.reset()
 env.render()
 print(env.observation_space)
 
+# Create the number of episodes
 episodes = 10
+
+# Loop through the episodes
 for episode in range(1, episodes+1):
+    # Reset the environment
     state = env.reset()
     done = False
     score = 0
+    n_state = []
+    action = 0
 
+    # Loop through the environment
     while not done:
-        env.render()
-        action = random.choice([0, 1])
+        # adjust action to make the model stable, make full use of the observation space
         n_state, reward, done, info, _ = env.step(action)
-        print(env.step(action))
+        env.render()
+        if n_state[2] > 0:
+            action = 1
+        else:
+            action = 0
+        state = n_state
         score += reward
-
-# close the window
+    print('Episode:{} Score:{}'.format(episode, score))
 env.close()
