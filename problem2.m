893e0a6d8e74642eef4579f44e0d231a8d36a279
mafumaful <s6bian@uwaterloo.ca>
2023-04-04 17:51:57 -0400

    pb2

--- problem2.m
+++ problem2.m
@@ -3,7 +3,7 @@ clc
 
 %% set the parameters
 % set the sampling time
-Ts = 0.01;
+Ts = 0.1;
 T_total = 10;
 % samples
 Nsim = T_total / Ts;
@@ -15,8 +15,13 @@ G = tf(1, [1 2 10]);
 [A, B, C, D] = ssdata(G);
 % create the model
 model = LTISystem('A', A, 'B', B, 'C', C, 'D', D, 'Ts', Ts);
-model.u.penalty = QuadFunction(diag(3));
-model.y.penalty = QuadFunction(diag(10));
+model.y.penalty = QuadFunction(diag(1));
+model.u.with('deltaMin');
+model.u.with('deltaMax');
+model.u.deltaMin = -1;
+model.u.deltaMax = 1;
+model.u.min = -10;
+model.u.max = 10;
 
 % set reference, in the first 5 seconds, the reference is 1, then it is 0
 yref = [ones(1, 5 / Ts), zeros(1, 5 / Ts)];
@@ -26,8 +31,18 @@ model.y.reference = 'free';
 ctrl = MPCController(model, 6);
 loop = ClosedLoop(ctrl, model);
 x0 = [0; 0];
+u0 = 0;
 
-data = loop.simulate(x0, Nsim, 'y.reference', yref);
+data = loop.simulate(x0, Nsim, 'u.previous', u0, 'y.reference', yref);
 
-plot(1:Nsim, data.Y);
-hold on;
+subplot(2, 1, 1); hold on;
+%% plot the results
+plot(1:Nsim, data.Y(1:Nsim), 'linewidth', 2);
+stairs(1:Nsim, yref, 'r--', 'linewidth', 2);
+axis([1, Nsim, -0.5, 1.5]);
+title('state')
+%% plot the input
+subplot(2, 1, 2); hold on;
+stairs(1:Nsim, data.U, 'linewidth', 2);
+axis([1, Nsim, -1, 12]);
+title('input')
